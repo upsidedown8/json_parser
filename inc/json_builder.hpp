98e71c1fe7ac74383728257dcb76d2a2ab2ef06a
upsidedown8 <65985223+upsidedown8@users.noreply.github.com>
2021-01-11 20:56:24 +0000

    create json_builder

---
+++ inc/json_builder.hpp
@@ -0,0 +1,27 @@
+#if !defined(JSONPARSER_JSONBUILDER_HPP)
+#define JSONPARSER_JSONBUILDER_HPP
+
+namespace json_parser {
+
+class JsonStringBuilder {
+
+};
+class JsonNumberBuilder {
+
+};
+class JsonBoolBuilder {
+
+};
+class JsonNullBuilder {
+
+};
+class JsonObjBuilder {
+
+};
+class JsonArrayBuilder {
+
+};
+
+}
+
+#endif // JSONPARSER_JSONBUILDER_HPP
